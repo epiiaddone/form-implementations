b0d08b19b2dbab41c2fbd8563adf011fe74b0ffe
epiiaddone <lowlandgiant@yahoo.com>
2023-07-28 12:37:43 +0800

    initial development completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {  ContactPage} from './ContactPage';
+ import { ThankYouPage } from './ThankYouPage';
+import { ContactPageUncontrolled } from './ContactPageUncontrolled';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate
+ } from 'react-router-dom';
+import { ContactPageRouter, contactPageRouterAction } from './ContactPageRouter';
+import { ContactPageNativeValidation } from './ContactPageNativevalidation';
+import { ContactPageReactHookForm } from './ContactPageReactHookForm';
+
+ const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <Navigate to="contact" />,
+  },
+  {
+  path: '/contact',
+  element: <>
+      <ContactPage/>
+      <ContactPageUncontrolled/>
+      <ContactPageRouter />
+      <ContactPageNativeValidation/>
+      <ContactPageReactHookForm/>
+  </>,
+  action: contactPageRouterAction,
+  },
+  {
+  path: '/thank-you/:name',
+  element: <ThankYouPage />,
+  },
+ ]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
